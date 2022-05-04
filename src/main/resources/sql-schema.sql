d2c8bad2fe256fe8d29c0266e3d3e4cdad005c22
guvmehat <gurveerm@hotmail.com>
2022-05-04 10:11:18 +0100

    IMS-14 #in-progress #comment created table in database for items

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     PRIMARY KEY (`id`)
 );
 
--- IMS 14
+-- IMS 14 
 CREATE TABLE IF NOT EXISTS `ims`.`items` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `item_name` VARCHAR(40) DEFAULT NULL,
