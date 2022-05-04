36103765a5a7f8c743aad3ae97554cfdc7e1aa06
guvmehat <gurveerm@hotmail.com>
2022-05-04 10:09:45 +0100

    IMS-14 #in-progress #commit created table in database fore items

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -12,3 +12,11 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+-- IMS 14
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) DEFAULT NULL,
+    `value` DOUBLE(6,2) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
